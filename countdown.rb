61af6b05398eff5a965b8842d0cef0dfefdc6753
Jerry Lee <jwl62@georgetown.edu>
2018-02-28 05:57:47 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(secs)
+  counter = secs
+  while counter >= 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+end
+
+def countdown_with_sleep(secs)
+  counter = secs
+  while counter >=0
+    puts "#{counter} SECOND(S)!"
+    sleep(1)
+    counter -= 1
+  end
 end
